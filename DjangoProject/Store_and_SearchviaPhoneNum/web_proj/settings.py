cd4ca7a9435ab313c15110ea3cc3a2fe1c859545
Aditya <123003281@sastra.ac.in>
2022-07-24 11:15:05 +0530

    New WebApp

--- DjangoProject/Store_and_SearchviaPhoneNum/web_proj/settings.py
+++ DjangoProject/Store_and_SearchviaPhoneNum/web_proj/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-h1f&=on)jxh9kdtv$*y=(q!b-=dauizmy9xi+9ux6g7vl5f07j'
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
